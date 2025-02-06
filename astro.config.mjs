7c097a5f705cc7cc33ad5ab3e47522088c67d545
VBproDev <proshowspeed@gmail.com>
2025-02-06 13:06:03 +0530

    added tagging of posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://frontendfrontier.netlify.app",
   experimental: {
     responsiveImages: true,
   },
