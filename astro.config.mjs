70b2eba59e5d1883bf288c2c0eb8688623b7d9a5
VBproDev <proshowspeed@gmail.com>
2025-02-27 10:24:51 +0530

    added dummy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://frontendfrontier.netlify.app",
+
   experimental: {
     responsiveImages: true,
   },
+
+  build: {
+     inlineStylesheets: 'never'
+  },
+
+  integrations: [purgecss()],
 });
